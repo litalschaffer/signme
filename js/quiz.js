68166e5de5d03dedabba11bbe8690d57dd508c20
Lital <litalschaffer@gmail.com>
2020-06-22 20:34:46 +0300

    more game updates

--- js/quiz.js
+++ js/quiz.js
@@ -39,33 +39,33 @@ let questions = [
 
 
 const qDictionary = [
-    ['how are you?','images/ques02.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['happy','images/VIDEO02.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['yes','images/VIDEO03.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['ok','images/VIDEO04.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['ok2','images/VIDEO05.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['day','images/VIDEO06.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['quiet','images/VIDEO07.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['gfb','images/VIDEO08.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['yes2','images/VIDEO09.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['how are you?2','images/VIDEO10.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['ok3','images/VIDEO11.jpg', 'https://youtu.be/OW3CdpmD3w4'],
-    ['feeling','images/VIDEO12.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
+    ['how are you?','images/ques2.jpg', 'https://youtu.be/OW3CdpmD3w4'],
   ]
 
 
 
 
 
-// const qVids = $('#qVideos')
+const qVids = $('#qVideos')
 
-// for (const entry of qDictionary) {
-//     const entryElem = $(`<div class="video" style="background-image: url(${entry[1]})">
-//             <a class="venobox" data-autoplay="true" data-vbtype="video" href="${entry[2]}" data-gall="myGallery">
-//             <i class="fas fa-play-circle"></i></a>
-//        </div>`)
-//     qVids.append(entryElem)
-// }
+for (const entry of qDictionary) {
+    const entryElem = $(`<div class="video" style="background-image: url(${entry[1]})">
+            <a class="venobox" data-autoplay="true" data-vbtype="video" href="${entry[2]}" data-gall="myGallery">
+            <i class="fas fa-play-circle"></i></a>
+       </div>`)
+    qVids.append(entryElem)
+}
 
 
 
